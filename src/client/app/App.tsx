07a97b1fb0ed003e04b5b99ed34a838f90e4a593
Trang Tran <ttminhtrang09@gmail.com>
2020-11-28 12:37:45 +0200

    Initial commit

--- src/client/app/App.tsx
+++ src/client/app/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import OpeningHours from '../openingHours/components/OpeningHours'
 import styles from './App.module.scss'
 
 const App = () => {
   return (
     <div className={styles.root}>
-      <OpeningHours />
+      APP TEMPLATE
     </div>
   )
 }
--- src/client/openingHours/components/InvalidDataView.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './InvalidDataView.module.scss'
-
-const InvalidDataView = () => {
-  return <div className={styles.root}>NO DATA OR INVALID DATA</div>
-}
-
-export default InvalidDataView
